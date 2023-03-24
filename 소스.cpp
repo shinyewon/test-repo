d83cabe8168821dac868479fd1adb7a1aef28f76
kootaeho <98327043+kootaeho@users.noreply.github.com>
2023-03-24 13:50:02 +0900

    Update 소스.cpp
    마지막 줄에 문자열 추가함

--- 소스.cpp
+++ 소스.cpp
@@ -9,5 +9,8 @@ void main() {
   string start = "--Start--\n";           //start 문자열 변수에 "--Start--" 할당
 	string greeting = "Hello world!\n";     //greeting 문자열 변수에 "Hello world!" 할당
 	string time = "It's lunch time now.\n"; //time 문자열 변수에 "It's lunch time now." 할당
-	cout << start << greeting << time;      //"--Start--", "Hello world!", "It's lunch time now." 문자열 출력
+	cout << start << greeting << time＜＜“ ｈａｖｅ ｎｉｃｅ ｌｕｎｃｈ¡”; //"--Start--", "Hello world!", "It's lunch time now." 문자열 출력
+	
+	
+	
 }
